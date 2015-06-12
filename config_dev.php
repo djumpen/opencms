30b49fe44fc3da8d39c3125da12935be493cd82d
Djumpen <demonitros@gmail.com>
2015-06-11 20:20:13 -0400

    From server

--- config_dev.php
+++ config_dev.php
@@ -35,9 +35,9 @@ define('STYLE_FOLDER', '/catalog/view/theme/default/stylesheet/');
 // DB
 define('DB_DRIVER', 'mpdo');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '7542');
-define('DB_DATABASE', 'freepeo_cms');
+define('DB_USERNAME', 'freepe_promo');
+define('DB_PASSWORD', 'KClKL1N?');
+define('DB_DATABASE', 'freepe_promo');
 define('DB_PREFIX', '');
 
 // OTHER
